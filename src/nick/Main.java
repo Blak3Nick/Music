1602297646963cb7c863652f8b734bc44a0192c1
Blak3Nick <blaken2021@gmail.com>
2018-11-13 14:39:20 -0500

    Initial commit

---
+++ src/nick/Main.java
@@ -0,0 +1,8 @@
+package nick;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/nick/model/DataSource.java
@@ -0,0 +1,8 @@
+package nick.model;
+
+public class DataSource {
+    public static final String DB_NAME = "music.db";
+
+    public static final String CONNECTION_STRING = "jdbc:sqlite:" + DB_NAME;
+
+}
