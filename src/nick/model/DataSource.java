2b42c14638991a3d4e403087dde05a88b035ab4c
Blak3Nick <blaken2021@gmail.com>
2018-11-13 14:42:42 -0500

    working with new table definitions

--- src/nick/model/DataSource.java
+++ src/nick/model/DataSource.java
@@ -5,4 +5,18 @@ public class DataSource {
 
     public static final String CONNECTION_STRING = "jdbc:sqlite:" + DB_NAME;
 
+    public static final String TABLE_ALBUMS = "albums";
+    public static final String COLUMN_ALBUM_ID = "_id";
+    public static final String COLUMN_ALBUM_NAME = "name";
+    public static final String COLUMN_ALBUM_ARTIST = "artist";
+
+    public static final String TABLE_ARTISTS = "artists";
+    public static final String COLUMN_ARTIST_ID = "_id";
+    public static final String COLUMN_ARTIST_NAME = "name";
+
+    public static final String TABLE_SONGS = "songs";
+    public static final String COLUMN_SONG_TRACK = "track";
+    public static final String COLUMN_SONG_TITLE = "title";
+    public static final String COLUMN_SONG_ALBUM = "album";
+
 }
